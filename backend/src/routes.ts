0f4ebe6b2eb6c336beca4ab33d49075b150de2ec
LucasGabryellll <gabryelmonteiro1@gmail.com>
2022-09-17 21:09:18 -0300

    Adicionando rotas do funcionario

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,15 @@
 import { Express, Router, Response, Request } from 'express';
 
-import { getAll_usuarios, create_usuario } from './useCases/usuario/controller';
+import {
+  getAll_usuarios,
+  create_usuario
+} from './useCases/usuario/controller';
+
+import {
+  getAll_Funcionarios,
+  getFuncionario_Cliente,
+  getFuncionario_Salario_AcimaMedia
+} from './useCases/funcionario/controller';
 
 const routes = Router();
 
@@ -8,7 +17,17 @@ routes.get('/', (request, response) => {
   return response.json("Hello word");
 })
 
+/**
+ * Rotas para a tabela usuario
+ */
 routes.get('/usuarios', getAll_usuarios);
 routes.post('/usuario/create_usuario', create_usuario);
+
+/**
+ * Rotas para a tabela funcionario
+ */
+routes.get('/funcionarios', getAll_Funcionarios);
+routes.get('/funcionario/cliente', getFuncionario_Cliente);
+routes.get('/funcionario/salario_acimaMedia', getFuncionario_Salario_AcimaMedia);
 
 export default routes;
