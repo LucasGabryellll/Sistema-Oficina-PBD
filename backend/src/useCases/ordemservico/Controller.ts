4cec166c561a6ea59034b414984d6364fa501aee
LucasGabryellll <gabryelmonteiro1@gmail.com>
2022-09-18 17:15:07 -0300

    Adicionando scripts para ordem de servico

--- backend/src/useCases/ordemservico/Controller.ts
+++ backend/src/useCases/ordemservico/Controller.ts
@@ -9,3 +9,19 @@ export const getAll_OrdemServicos = async (request: Request, response: Response)
   const veiculos = await dao.getAll_OrdemServicos();
   return response.json(veiculos);
 }
+
+/**
+ * Retorna os clientes não atendidos
+ */
+ export const getClientesNaoAtendidos = async (request: Request, response: Response) => {
+
+  try {
+    const clientesNaoAtendidos = await dao.getClientesNaoAtendidos();
+    return response.json(clientesNaoAtendidos);
+
+  } catch (error) {
+    return response.status(400).json({
+      message: "Erro ao buscar"
+    })
+  }
+}
--- backend/src/useCases/ordemservico/Dao.ts
+++ backend/src/useCases/ordemservico/Dao.ts
@@ -1,22 +1,30 @@
 import { Connection, ConnectDB } from '../../ConnectionDB/connection';
 import { OrdemServico } from "./Model";
 
-const _getAllOrdemservicos= () => { return 'SELECT * FROM ORDEMSERVICO' }
+const _getAllOrdemservicos = () => { return 'SELECT * FROM ORDEMSERVICO' }
 
-
-
+const _getClientesNaoAtendidos = () => {
+  return `select o.id, v.placa, c.nome, c.cpfcnpj, o.status from veiculo v, cliente c, ordemservico o where  o.status = 'não concluido' and v.id_cliente = c.id and o.id_cliente = c.id`
+}
 
 export class OrdemServicoDao {
-    constructor(
-  
-    ) { }
-  
-    async getAll_OrdemServicos() {
-      const cursor = Connection();
-      cursor.connect();
-      const data = await cursor.query(_getAllOrdemservicos())
-      cursor.end();
-      return data.rows
-    }
-  
+  constructor(
+
+  ) { }
+
+  async getAll_OrdemServicos() {
+    const cursor = Connection();
+    cursor.connect();
+    const data = await cursor.query(_getAllOrdemservicos())
+    cursor.end();
+    return data.rows
   }
+
+  async getClientesNaoAtendidos() {
+    const cursor = Connection();
+    cursor.connect();
+    const data = await cursor.query(_getClientesNaoAtendidos())
+    cursor.end();
+    return data.rows
+  }
+}
